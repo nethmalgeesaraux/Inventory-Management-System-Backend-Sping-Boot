4e499fb9735c80ea4f3432efea53c87fcb5a7652
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-06 20:04:41 +0530

    user Dto Entitys

--- src/main/java/com/example/Inventory/System/models/user.java
+++ src/main/java/com/example/Inventory/System/models/user.java
@@ -1,15 +1,46 @@
 package com.example.Inventory.System.models;
 
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table (name = "users")
+@Builder
 public class user {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
-    private String password;
-    private String phoneNumber;
-    private String role;
-    private String transition;
 
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Column
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "PhoneNumber is required")
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Transaction> transactions;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
 
 }
