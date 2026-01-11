b86e91d7e5ffd18e8e6d4945dc962a37ae4e0dd0
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-11 22:47:24 +0530

    CategoryDto

---
+++ src/main/java/com/example/Inventory/System/dtos/CategoryDTO.java
@@ -0,0 +1,25 @@
+package com.example.Inventory.System.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CategoryDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private List<ProductDTO> products;
+}
