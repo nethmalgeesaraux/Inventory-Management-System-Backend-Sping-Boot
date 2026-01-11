5a29b83348702e24b8016323470cd32960d68330
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-11 23:22:03 +0530

    SupplierDTO

---
+++ src/main/java/com/example/Inventory/System/dtos/SupplierDTO.java
@@ -0,0 +1,29 @@
+package com.example.Inventory.System.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SupplierDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = " contactInfo is required")
+    private String contactInfo;
+
+    private String address;
+
+}
