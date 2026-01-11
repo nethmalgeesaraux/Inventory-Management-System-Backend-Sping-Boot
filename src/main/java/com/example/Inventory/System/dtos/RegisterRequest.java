2d113895e38de32305bebee6f071924a9752bc3f
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-11 23:17:04 +0530

    RegisterRequest

---
+++ src/main/java/com/example/Inventory/System/dtos/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example.Inventory.System.dtos;
+
+import com.example.Inventory.System.enums.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "phoneNumber is required")
+    private String phoneNumber;
+
+    private UserRole role;
+}
