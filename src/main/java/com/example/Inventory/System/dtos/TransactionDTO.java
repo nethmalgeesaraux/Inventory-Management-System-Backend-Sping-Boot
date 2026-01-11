19caee58f609544ee009d3c624d95c9064ba5c7b
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-11 23:25:40 +0530

    TransactionDTO

---
+++ src/main/java/com/example/Inventory/System/dtos/TransactionDTO.java
@@ -0,0 +1,46 @@
+package com.example.Inventory.System.dtos;
+
+import com.example.Inventory.System.enums.TransactionStatus;
+import com.example.Inventory.System.enums.TransactionType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionDTO {
+
+    private Long id;
+
+    private Integer totalProducts;
+
+    private BigDecimal totalPrice;
+
+
+    private TransactionType transactionType; // pruchase, sale, return
+
+
+    private TransactionStatus status; //pending, completed, processing
+
+    private String description;
+    private String note;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
+
+    private ProductDTO product;
+
+    private UserDTO user;
+
+    private SupplierDTO supplier;
+}
