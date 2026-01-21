1887ad91290927bc6d389f8866e65b2233faa795
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-01-21 17:35:11 +0530

    ProductService

---
+++ src/main/java/com/example/Inventory/System/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.Inventory.System.services;
+
+import com.example.Inventory.System.dtos.ProductDTO;
+import com.example.Inventory.System.dtos.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ProductService {
+    Response saveProduct(ProductDTO productDTO, MultipartFile imageFile);
+
+    Response updateProduct(ProductDTO productDTO, MultipartFile imageFile);
+
+    Response getAllProducts();
+
+    Response getProductById(Long id);
+
+    Response deleteProduct(Long id);
+
+    Response searchProduct(String input);
+}
